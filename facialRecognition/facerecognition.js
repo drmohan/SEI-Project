c61d648659b1c35629a5182354a8ea4ff3c438e9
Katie Williams <kaw1@andrew.cmu.edu>
2017-02-22 19:25:58 -0500

    Integrated box around face with distance to screen

--- facialRecognition/facerecognition.js
+++ facialRecognition/facerecognition.js
@@ -1,3 +1,5 @@
+
+// get media from browser (video) with getUserMedia()
 navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
    getUserMedia: function(c) {
      return new Promise(function(y, n) {
@@ -10,6 +12,7 @@ if (!navigator.mediaDevices) {
   throw new Error("getUserMedia() not supported.");
 }
 
+// access HTML elements
 var video = document.querySelector('video');
 var constraints = {
   audio: false,
@@ -17,6 +20,24 @@ var constraints = {
 };
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
+
+// data collection for proximity to screen
+var percentages = [];
+
+// functions to handle input numbers for face proximity to screen
+function rollingAverage(size) {
+  percentages.splice(0, percentages.length - size);
+  var sum = percentages.reduce(function(total, num) {
+    return total + num
+  }, 0);
+  return sum / percentages.length;
+}
+
+function percentageToInches(p) {
+  return 49 * Math.exp(-0.023 * p);
+}
+
+// camera display function
 function startCamera() {
   return navigator.mediaDevices.getUserMedia(constraints)
   .then(function(stream) {
@@ -31,15 +52,20 @@ function startCamera() {
         min_neighbors: 1
       });
       faces.forEach(function(face) {
+        // draw rectangle around face
         context.beginPath();
         context.rect(face.x, face.y, face.width, face.height);
         context.lineWidth = 1;
         context.strokeStyle = 'red';
         context.stroke();
+        // write inches to screen in code element
+        var percentage = 100 * face.height / canvas.height;
+        percentages.push(percentage);
+        document.querySelector('code').textContent = percentageToInches(rollingAverage(5)).toFixed(0) + '"';
        });
-    }, 100);
+    }, 100); // or 300
 
-    return stream;  // so chained promises can benefit
+    return stream; 
   })
   .catch(function(error) {
     console.error(error);
@@ -50,7 +76,7 @@ document.querySelector('button').onclick = function() {
   var button = this;
   if (streamOn !== null) {
     streamOn.stop();
-    URL.revokeObjectURL(video.src);  // cleanin up
+    URL.revokeObjectURL(video.src);  
     button.textContent = 'Start camera';
   } else {
     button.textContent = 'Starting camera';
