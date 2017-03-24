7efb1a4a0a419f8373ece7236b5b42a794656295
Katie Williams <kaw1@andrew.cmu.edu>
2017-03-24 02:21:34 -0400

    Created first page text animation and updated style to see it better

--- main/assets/js/intro.js
+++ main/assets/js/intro.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
 
-//     // fade 'Welcome'
-//     $('.green.leaf').transition({
-//         animation  : 'scale',
-//         duration   : '2s',
-//         onComplete : function() {
-//             alert('done');
-//     }
-//   })
-// ;
-//     $('.fade-in-welcome').transition('fade')
+    // PAGE 1
+
+    // Hide Intro Text at beginning
+    $('.intro-word')
+        .transition('hide');
+    
     $('.fade-in-welcome').transition({
         animation: 'fade in',
-        duration: '2s'
+        duration: '3s'
     });
 
-    $('.word').transition({
+    $('.intro-word').data('hideInterval', setTimeout(function(){
+        $('.intro-word')
+    .transition({
         animation: 'slide in right',
-        duration: '4s',
+        duration: '3s',
         interval: 200
     });
+    }, 2000))
 
 
+    // slide arrow in
+
+    // show 'see demo' and 'skip intro'
+
+    // bounce 'see demo' once
+
+    // TRANSITION PAGES - page 1 to page 2
+
+    // move text and arrow up and to left
+
+
+    
+    // PAGE 2
+
+    // show text until 'your heartbeat' faster
+
+    // show 'your heartbeat' on rhythm with visual graphic
+
+    // show heartbeat graphic
+
+    // show text 'by looking at you' faster
+
+    // show arrow
 
     // fade in right introduction text 1 ('Team Intro')
       var self = this;
