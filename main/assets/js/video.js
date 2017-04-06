3e08cf5ffe5e473e855288d1498ea5c4437fa621
Katie Williams <kaw1@andrew.cmu.edu>
2017-04-05 23:39:15 -0400

    Integrated distance technology from facialRecognition folder to give inches from screen

--- main/assets/js/video.js
+++ main/assets/js/video.js
@@ -26,6 +26,23 @@ tracker.setInitialScale(4);
 tracker.setStepSize(2);
 tracker.setEdgesDensity(0.1);
 
+// data collection for proximity to screen
+var percentages = [];
+
+// functions to handle input numbers for face proximity to screen
+function rollingAverage(size) {
+  percentages.splice(0, percentages.length - size);
+  var sum = percentages.reduce(function(total, num) {
+    return total + num
+  }, 0);
+  return sum / percentages.length;
+}
+
+function percentageToInches(p) {
+  return 49 * Math.exp(-0.023 * p);
+}
+
+
 tracking.track('video#live', tracker, { camera: true });
 
 tracker.on('track', function(event) {
@@ -38,8 +55,14 @@ tracker.on('track', function(event) {
       context.fillStyle = "#fff";
       context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
       context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+        
+      // write inches to screen in code element
+    var percentage = 100 * rect.height / canvas.height;
+    percentages.push(percentage);
+    document.querySelector('code').textContent = percentageToInches(rollingAverage(5)).toFixed(0) + '"';
     });
 });
+
 
 var recordButton = document.querySelector('button#record');
 var playButton = document.querySelector('button#play');
@@ -159,7 +182,7 @@ function play() {
 
 function download() {
 
-  var IP = '128.237.218.42';
+  var IP = '128.237.216.124';
   var port = 8888;
   var socket = io.connect('http://' + IP + ':' + port);
 
