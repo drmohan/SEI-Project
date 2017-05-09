38e6f505fcca3ee64cd789cb48d09d8f12ff3d82
Katie Williams <kaw1@andrew.cmu.edu>
2017-05-08 23:55:21 -0400

    flipped video image so more intuitive to user, reformatted contact form

--- main/contact.php
+++ main/contact.php
@@ -2,7 +2,7 @@
 if(isset($_POST['send'])) {
     header("Location: "."http://localhost:8000/");  
     $subject = $_POST['Interest'];
-    $message = $_POST['firstName'] . " " . $_POST['lastName'] . " " . $_POST['emailAddress'];
+    $message = $_POST['Name'] . " " . $_POST['emailAddress'];
     mail("asawarikanitkar@gmail.com", $subject, $message, "From: cmuform@gmail.com");
        
 }
