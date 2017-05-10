08113dc5da1991aea8da3fdc7fb50d34068c2847
Divya <drmohan@andrew.cmu.edu>
2017-05-10 13:44:11 -0400

    fixed view results glitch

--- main/assets/js/video.js
+++ main/assets/js/video.js
@@ -318,7 +318,7 @@ cancelButton.onclick = function(event) {
 function sendData() {
 
   var x = showForm()
-  var IP = '10.0.0.61';
+  var IP = '128.237.184.93';
   var port = 8888;
   var socket = io.connect('http://' + IP + ':' + port);
 
